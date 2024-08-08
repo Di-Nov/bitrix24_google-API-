948f04e2d83933d165933e1b83681b7b7c4c0628
Di-Nov <pieero47@gmail.com>
2024-08-08 19:48:38 +0300

    Создание кнопки для Битрикс24

---
+++ button.py
@@ -0,0 +1,26 @@
+button = '''
+        <html>
+            <head>
+                <style>
+                    .button {
+                        display: inline-block;
+                        padding: 10px 20px;
+                        font-size: 16px;
+                        color: white;
+                        background-color: #4CAF50; /* Зеленый цвет */
+                        border: none;
+                        border-radius: 5px;
+                        text-align: center;
+                        text-decoration: none;
+                        transition: background-color 0.3s;
+                    }
+                    .button:hover {
+                        background-color: #45a049; /* Темнее при наведении */
+                    }
+                </style>
+            </head>
+            <body>
+                <a href="/login" class="button">Get users and load in Google Sheets</a>
+            </body>
+        </html>
+    '''
